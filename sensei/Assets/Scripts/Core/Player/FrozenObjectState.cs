a0cda1b11db68528ebc303a5b8c0a9ef65c9e743
Kacper <Acymon@gmail.com>
2020-01-25 19:06:53 +0100

    update frozen state

--- sensei/Assets/Scripts/Core/Player/FrozenObjectState.cs
+++ sensei/Assets/Scripts/Core/Player/FrozenObjectState.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UniRx.Async;
+using System;
 
 namespace Core.Player
 {
@@ -6,6 +8,10 @@ namespace Core.Player
     {
         private Vector3 savedVelocity;
         private Vector3 savedAngularVelocity;
+    
+        Material objMaterial;
+        Material originalMateial;
+        Color originalColor;
 
         private void Start()
         {
@@ -16,7 +22,43 @@ namespace Core.Player
                 savedAngularVelocity = rb.angularVelocity;
                 rb.isKinematic = true;
             }
+
+            this.startBlinking();
         }
+
+
+        public async UniTask startBlinking() {
+            this.objMaterial = GetComponent<MeshRenderer>().material;
+            this.originalMateial = GetComponent<MeshRenderer>().material;
+            this.originalColor = GetComponent<MeshRenderer>().material.color;
+
+            await blinkForSeconds(2f);
+            await blinkForSeconds(1.6f);
+            await blinkForSeconds(0.5f);
+            await blinkForSeconds(0.3f);
+            await blinkForSeconds(0.2f);
+            await blinkForSeconds(0.1f);
+            await blinkForSeconds(0.1f);
+            await blinkForSeconds(0.1f);
+            await blinkForSeconds(0.1f);
+            await blinkForSeconds(0.1f);
+            await blinkForSeconds(0.1f);
+            await blinkForSeconds(0.1f);
+            await blinkForSeconds(0.1f);
+
+            Destroy(this);
+        }
+
+    private async UniTask blinkForSeconds(float firstColor, float secondColor = 0.05f) {
+        objMaterial.color = Color.magenta;
+
+        await UniTask.Delay(TimeSpan.FromSeconds(firstColor), ignoreTimeScale: false);
+
+        objMaterial.color = this.originalColor;
+
+        await UniTask.Delay(TimeSpan.FromSeconds(secondColor), ignoreTimeScale: false);
+
+    }
 
         private void OnDestroy()
         {

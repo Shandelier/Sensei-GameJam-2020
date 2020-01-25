3c7e74e4be5f0db8329244daa9e3d480b26eca3b
jakub.mikicki <jakub.mikicki@tensquaregames.com>
2020-01-25 19:02:22 +0100

    dupa dupa raz raz

---
+++ sensei/Assets/Scripts/Core/SoundManager/Spotify.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.SoundManager{
+    public class Spotify : MonoBehaviour
+    {
+        [SerializeField]
+        private bool autoplay = true; 
+        
+        void Start(){
+            if(autoplay && AudioAssets.Get.musicClipsArray != null){
+                SoundManager.PlayMusic(SoundManager.Music.idleAmbient);
+            }
+        }
+    }
+}

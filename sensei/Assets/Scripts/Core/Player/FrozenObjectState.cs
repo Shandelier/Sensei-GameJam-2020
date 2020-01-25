a32137163c524bb3332af27aa7bd203b4641e0b3
Carlos <asmonix@gmail.com>
2020-01-25 12:30:39 +0100

    preserve velocity after save

---
+++ sensei/Assets/Scripts/Core/Player/FrozenObjectState.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Core.Player
+{
+    public class FrozenObjectState : MonoBehaviour
+    {
+        private Vector3 savedVelocity;
+
+        private void Start()
+        {
+            var rb = GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                savedVelocity = rb.velocity;
+                rb.isKinematic = true;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            var rb = GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.isKinematic = false;
+                rb.velocity = savedVelocity;
+                Debug.Log($"velocity {savedVelocity}");
+            }
+        }
+    }
+}

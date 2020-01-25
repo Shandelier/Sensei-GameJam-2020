411517a6eca5cb76d317d377b5fc7d8e56537e50
Mateusz Osowski <mateusz.osowski@tensquaregames.com>
2020-01-25 21:17:27 +0100

    Trampoline level

---
+++ sensei/Assets/Scripts/Core/WorldPuzzle/ApplyRandomForce.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyRandomForce : MonoBehaviour
+{
+    public float speed = 1;
+    public float strength = 1;
+    
+    float time;
+    Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(new Vector3(
+            Noise.SimplexValue1D(Vector3.up * time, 1).value,
+            Noise.SimplexValue1D(Vector3.right * time, 1).value,
+            Noise.SimplexValue1D(Vector3.forward * time, 1).value
+        ) * strength);
+        
+        time += Time.deltaTime * speed;
+    }
+}

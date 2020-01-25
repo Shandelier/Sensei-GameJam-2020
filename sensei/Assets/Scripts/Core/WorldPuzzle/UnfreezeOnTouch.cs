e9fd8b489cb85d5cf8b717da0329eeeafdac38b1
Mateusz Osowski <mateusz.osowski@tensquaregames.com>
2020-01-25 12:56:30 +0100

    Stairs + unfreeze on touch

---
+++ sensei/Assets/Scripts/Core/WorldPuzzle/UnfreezeOnTouch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnfreezeOnTouch : MonoBehaviour
+{
+    public GameObject target;
+    public Vector3 impulse;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (target == null)
+        {
+            target = gameObject;
+        }
+        var targetRb = target.GetComponent<Rigidbody>();
+        if (targetRb == null)
+        {
+            targetRb = target.AddComponent<Rigidbody>();
+        }
+
+        targetRb.isKinematic = false;
+        targetRb.AddForce(impulse, ForceMode.Impulse);
+    }
+}

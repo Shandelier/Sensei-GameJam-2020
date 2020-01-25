3afe7e957f2b36efce0707cf611a989a3f323957
Kacper Burzała <234943@student.pwr.edu.pl>
2020-01-25 11:18:52 +0100

    Freezing objects

--- sensei/Assets/Scripts/Core/Player/SelectionManager.cs
+++ sensei/Assets/Scripts/Core/Player/SelectionManager.cs
@@ -7,6 +7,9 @@ public class SelectionManager : MonoBehaviour
 
     private Material defaultMaterial;
     private Transform _selection;
+
+    private Vector3 mOffset;
+    private float mZCoord;
     
     private void Update()
     {
@@ -25,14 +28,37 @@ public class SelectionManager : MonoBehaviour
             if (selection.CompareTag(selectableTag))
             {
                 var selectionRenderer = selection.GetComponent<Renderer>();
-                if (selectionRenderer != null)
+                if (selectionRenderer != null && !selectionRenderer.GetComponent<Rigidbody>().isKinematic)
                 {
                     defaultMaterial = selectionRenderer.material;
                     selectionRenderer.material = highlightMaterial;
                 }
 
+                if (Input.GetKeyDown("f"))
+                {
+                    if (selectionRenderer.GetComponent<Rigidbody>().isKinematic)
+                        selectionRenderer.GetComponent<Rigidbody>().isKinematic = false;
+                    else
+                        selectionRenderer.GetComponent<Rigidbody>().isKinematic = true;
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    mZCoord = Camera.main.WorldToScreenPoint(selectionRenderer.transform.position).z;
+                    mOffset = selectionRenderer.transform.position - GetMouseWorldPos();
+                }
+
                 _selection = selection;
             }
         }
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 }

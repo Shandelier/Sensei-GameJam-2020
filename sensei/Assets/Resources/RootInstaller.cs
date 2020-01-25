e2746e7af3ed79f57f4d57de0bcc2784bb98e23f
Kacper <Acymon@gmail.com>
2020-01-25 03:11:36 +0100

    Ayy lmao

---
+++ sensei/Assets/Resources/RootInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class RootInstaller : MonoInstaller
+{
+
+    public override void InstallBindings()
+    {
+        this.installInputBindings();
+
+    }
+
+    private void installInputBindings() {
+        
+    }
+
+}

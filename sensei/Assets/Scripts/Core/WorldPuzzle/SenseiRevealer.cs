1951ad9f64c2467f32fe2832f68b7330c87a93a3
Mateusz Osowski <mateusz.osowski@tensquaregames.com>
2020-01-26 10:28:12 +0100

    Sensei monument

---
+++ sensei/Assets/Scripts/Core/WorldPuzzle/SenseiRevealer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SenseiRevealer : MonoBehaviour
+{
+    public GameObject sensei;
+
+    Material material;
+    bool hasStartedRevealing = false;
+    float startTime = -999;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerTag")
+        {
+            Reveal();
+        }
+    }
+
+
+    void Start()
+    {
+        var render = sensei.GetComponent<MeshRenderer>();
+        render.material = material = Instantiate(render.material);
+    }
+
+    public void Reveal()
+    {
+        startTime = Time.time;
+        hasStartedRevealing = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasStartedRevealing)
+        {
+            material.SetFloat("_Reveal", Time.time - startTime);
+        }
+    }
+}

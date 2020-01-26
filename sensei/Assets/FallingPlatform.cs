4360f434896765747f895b21033c603129a39c74
Kacper <Acymon@gmail.com>
2020-01-26 12:34:03 +0100

    finalScenes

---
+++ sensei/Assets/FallingPlatform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx.Async;
+
+public class FallingPlatform : MonoBehaviour
+{
+    Vector3 originalPosition;
+    Transform transform;
+    bool isFalling = false;
+
+    void Start() {
+        this.transform = this.GetComponent<Transform>();
+        this.originalPosition = this.transform.position;
+    }
+
+    void OnCollisionEnter(Collision other) {
+
+        if(!this.isFalling) {
+            this.isFalling = true;
+            this.fallDown();
+        }
+        
+    }
+
+    private async UniTask fallDown() {
+        Debug.Log("bbbbb");
+        Rigidbody objRigibdoby = this.GetComponent<Rigidbody>();
+        await UniTask.Delay(1000, ignoreTimeScale: false);
+
+        objRigibdoby.isKinematic = false;
+        objRigibdoby.useGravity = true;
+        objRigibdoby.AddForce(new Vector3(0, -100, 0), ForceMode.Impulse);
+
+        await UniTask.Delay(10000, ignoreTimeScale: false);;
+
+        this.isFalling = false;
+        objRigibdoby.useGravity = false;
+        objRigibdoby.isKinematic = true;
+        this.transform.position = this.originalPosition;
+        
+    }
+}
